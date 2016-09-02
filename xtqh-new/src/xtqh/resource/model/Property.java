dae4c7ca1beeeaff2a67975de0736bc74ea6b068
jumpingwu <jumpingwu@gmail.com>
2016-09-02 21:48:34 +0800

    updated Property.java

--- xtqh-new/src/xtqh/resource/model/Property.java
+++ xtqh-new/src/xtqh/resource/model/Property.java
@@ -1,15 +1,16 @@
 package xtqh.resource.model;
 
+import xtqh.base.model.BaseEntity;
 
 /**
  * @author Daniel
  * @version 1.0
  * @created 02-九月-2016 21:00:42
  */
-public class Property {
+public class Property extends BaseEntity {
 
 	private String value;
-	private List<ScriptArgument> scriptArguments;
+//	private List<ScriptArgument> scriptArguments;
 
 	public Property(){
 
